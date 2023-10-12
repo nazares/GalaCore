5d92d89119d6f1638eaae1383e6c0fa5e6caa036
Sergei Nazarenko <nazares@icloud.com>
2023-10-12 13:12:06 +0300

    add symfony http component

---
+++ src/Gala/Http/ResponseHandler.php
@@ -0,0 +1,21 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gala\Http;
+
+use Symfony\Component\HttpFoundation\Response;
+
+class ResponseHandler
+{
+    public function handler(): Response
+    {
+        if (!isset($response)) {
+            $response = new Response();
+            if ($response) {
+                return $response;
+            }
+        }
+        return false;
+    }
+}
