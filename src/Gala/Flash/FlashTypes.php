5131fbb8ae0b75fcb3800405d09aed4cdb326b20
Sergei Nazarenko <nazares@icloud.com>
2023-10-12 13:11:17 +0300

    add Flash component

---
+++ src/Gala/Flash/FlashTypes.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gala\Flash;
+
+class FlashTypes
+{
+    public const SUCCESS = 'success';
+    public const WARNING = 'warning';
+    public const DANGER = 'danger';
+    public const INFO = 'info';
+}
