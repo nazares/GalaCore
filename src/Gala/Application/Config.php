a0984fc6450cd5a23b0774c73d81092b329ed2e7
Sergei Nazarenko <nazares@icloud.com>
2023-10-12 13:14:36 +0300

    add Config

---
+++ src/Gala/Application/Config.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gala\Application;
+
+class Config
+{
+    public const GALA_MIN_VERSION = '8.0.30';
+
+    public const GALA_CORE_VERSION = "1.0.0";
+}
