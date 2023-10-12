ed64a5bf371f351140ce373d14812b99eed02d9a
Sergei Nazarenko <nazares@icloud.com>
2023-10-12 13:08:37 +0300

    added Twig component;

---
+++ src/Gala/Twig/TwigExtension.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Gala\Twig;
+
+use Twig\Extension\AbstractExtension;
+use Twig\Extension\GlobalsInterface;
+
+class TwigExtension extends AbstractExtension implements GlobalsInterface
+{
+    //
+}
